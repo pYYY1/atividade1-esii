f5535dcd8a97f7b3059e7bd7399da11e6583d66d
rafael-ulguim1 <rv862086@gmail.com>
2024-04-15 19:37:56 -0300

    Implementação da feature da divisão
    retorna o valor da divisão entre os dois parâmetros

--- calculator.js
+++ calculator.js
@@ -1,21 +1,27 @@
 // retorna o valor da soma entre os dois parâmetros
 // Gustavo Py
-function sum(v1, v2) {}
+function sum(v1, v2) { }
 
 // retorna o valor da subtração entre os dois parâmetros
 // Victor
-function sub(v1, v2) {}
+function sub(v1, v2) { }
 
 // retorna o valor da divisão entre os dois parâmetros
 // Rafael
-function div(v1, v2) {}
+function div(v1, v2) {
+    if (v2 === 0) {
+        return "Erro: divisão por zero";
+    } else {
+        return v1 / v2;
+    }
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 // Erson
-function mult(v1, v2) {}
+function mult(v1, v2) { }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 // Manu
-function square(v1) {}
+function square(v1) { }
 
 
